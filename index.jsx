1eadc4d69d27587dbd5efe8756d60d88de60eca5
Jonah Zimmer <jszimmer545@gmail.com>
2024-10-03 15:56:50 -0500

    embed and format igl iframe

--- index.jsx
+++ index.jsx
@@ -1,20 +1,8 @@
-var fs = require("fs");
-var browserify = require("browserify");
-var babelify = require("babelify");
-
-browserify({ debug: true })
-  .transform(babelify)
-  .require("./script.js", { entry: true })
-  .bundle()
-  .on("error", function (err) { console.log("Error: " + err.message); })
-  .pipe(fs.createWriteStream("bundle.js"));
-
-import { createRoot } from 'react-dom/client';
+console.log("hello");
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById('react'));
 root.render(<h1>Hello, world</h1>);
-
