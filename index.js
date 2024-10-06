cb725210ef121ddfe276885cc05b7c100d2a255c
Jonah Zimmer <jszimmer545@gmail.com>
2024-10-06 12:35:31 -0500

    use react to create landing page modules

---
+++ index.js
@@ -0,0 +1,4 @@
+import LandingPage from './index.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('homePageAllNavs'));
+root.render(<LandingPage />);
--- index.jsx
+++ index.jsx
@@ -1,8 +1,39 @@
-console.log("hello");
+import React from 'react';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+class Simulation {
+    name;
+    shortName;
+    link;
+    p5js;
+    description;
 
-// Render your React component instead
-const root = createRoot(document.getElementById('react'));
-root.render(<h1>Hello, world</h1>);
+    constructor(name, shortName, link, p5js, description) {
+        this.name = name;
+        this.shortName = shortName;
+        this.link = link;
+        this.p5js = p5js;
+        this.description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse illo earum, recusandae est fuga harum ipsa nisi cupiditate reprehenderit perspiciatis voluptatum velit impedit aliquam deleniti sapiente in dicta sit molestiae!";
+    }
+};
+
+const igl = new Simulation("Ideal Gas Law", "igl", "igl.html", "https://editor.p5js.org/bresypedraza/full/51SPYMqzt", "");
+const orbital = new Simulation("Orbital Combinations", "orbital", "orbital.html", "", "");
+let simulations = [igl, orbital];
+
+function SimulationModule (s) {
+    let imgId = s.shortName.concat("NavImg");
+    let key = s.shortName.concat("Module");
+    return (<div key={key} className="homePageNavContainer">
+        <a className="homePageNavLink" href={s.link}>
+            <div className="homePageNavImg" id={imgId}>
+                <div className="homePageNavLabel">{s.name}</div>
+            </div>
+            <div className="homePageNavDescr">{s.description}</div>
+        </a>
+    </div>);
+}
+
+export default function LandingPage () {
+    console.log(simulations.map(SimulationModule));
+    return simulations.map(SimulationModule);
+}
